282f587cd7412f16a942ae804149a5b4f738a8d0
Dhandapani Sudhakar <dhandapani.sudhakar@outlook.com>
2025-02-15 14:22:35 +0530

    Use h2 db and application-test.properties for integration testing

--- dockerize-spring-boot-mysql/src/test/java/com/dhandapani/dockerize_spring_boot_mysql/DockerizeSpringBootMysqlApplicationTests.java
+++ dockerize-spring-boot-mysql/src/test/java/com/dhandapani/dockerize_spring_boot_mysql/DockerizeSpringBootMysqlApplicationTests.java
@@ -3,7 +3,7 @@ package com.dhandapani.dockerize_spring_boot_mysql;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.config.location=classpath:application-test.properties")
 class DockerizeSpringBootMysqlApplicationTests {
 
 	@Test
